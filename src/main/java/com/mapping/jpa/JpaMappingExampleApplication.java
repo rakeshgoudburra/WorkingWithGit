92a4ce061cee64d1c409897c81c78dd6e4a9c5e1
rakeshgoudburra <103447116+rakeshgoudburra@users.noreply.github.com>
2022-08-25 22:36:39 +0530

    Update JpaMappingExampleApplication.java

--- src/main/java/com/mapping/jpa/JpaMappingExampleApplication.java
+++ src/main/java/com/mapping/jpa/JpaMappingExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpaMappingExampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("changed in Git hub");
 		SpringApplication.run(JpaMappingExampleApplication.class, args);
 	}
 
